d3c15cdbc8c62e45fea44c7203dc47a5dc6f5305
farideghbali <farid.eghbalii@gmail.com>
2022-11-15 17:07:28 +0330

    upload project on github

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"], darkMode: "class", theme: {
+        extend: {
+            colors: {
+                "slate-800": "rgb(29,33,68)",
+                "backgrount-input": "rgb(36,43,81)",
+                "gray-dark": "rgba(74,108,247,.1)",
+                "dark": "rgb(9, 14, 52)"
+            }, boxShadow: {
+                "3xl": "0px 5px 10px rgba(4, 10, 34, .2)"
+            }
+        }, fontFamily: {
+            "Inter": '"Inter", sans-serif'
+        },
+    }, plugins: [],
+}
