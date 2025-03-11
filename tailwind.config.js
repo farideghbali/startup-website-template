39ace5cb02f720bd2f840785cdeddec5a32b7b5e
Farid Eghbali <58879964+farideghbali@users.noreply.github.com>
2025-03-12 00:50:01 +0330

    add OG image, fix typos, and improve clean code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html"], darkMode: "class", theme: {
+    content: ["./index.html"], darkMode: "class",
+    theme: {
         extend: {
             colors: {
                 "slate-800": "rgb(29,33,68)",
-                "backgrount-input": "rgb(36,43,81)",
+                "background-input": "rgb(36,43,81)",
                 "gray-dark": "rgba(74,108,247,.1)",
                 "dark": "rgb(9, 14, 52)"
             }, boxShadow: {
